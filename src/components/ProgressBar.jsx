6e25f6ab8894c085022804a42683da723bf59c14
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-07-11 16:37:11 +0200

    first commit

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function ProgressBar({ timer }) {
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div>
+      <progress max={timer} value={remainingTime}></progress>
+    </div>
+  );
+}
